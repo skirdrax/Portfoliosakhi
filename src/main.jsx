f26354c9f0e02bad0dd6acbcd8646b26815f75e1
sakhiard <ardrasakhi390@gmail.com>
2026-01-06 23:29:20 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import Navbar from './components/Navbar.jsx';
-import Footer from './components/footer.jsx';
-import PreLoader from './components/PreLoader.jsx';
-import 'animate.css';
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
-// ..
-AOS.init();
+import Footer from './components/Footer.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <>
-      <PreLoader />
       <div className="container mx-auto px-4">
         <Navbar />
         <App />
