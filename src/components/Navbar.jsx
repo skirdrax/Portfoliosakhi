4fffa8658a10a169e599b08b16b223feb8772d3f
sakhiard <ardrasakhi390@gmail.com>
2026-05-10 15:46:09 +0700

    update besar 4

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -81,7 +81,7 @@ export default function Navbar() {
               </a>
             ))}
             <a
-              href="https://drive.google.com/file/d/1a04mQmiaDVA0lyF0PSGjMaXoi07NDWDB/view?usp=drive_link"
+              href="https://drive.google.com/file/d/1VG8iLlGfUZkEZAesSKNrxbj_mQuLO2tT/view?usp=drive_link"
               className="nav-cta"
               target="_blank"
               rel="noopener noreferrer">
