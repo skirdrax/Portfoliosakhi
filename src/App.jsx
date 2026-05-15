b5cee2ba5b636612919ee47062d4a9a3e777aad8
sakhiard <ardrasakhi390@gmail.com>
2026-05-15 20:03:33 +0700

    update fix 6 github foto

--- src/App.jsx
+++ src/App.jsx
@@ -660,7 +660,7 @@ export default function App() {
             <h2 className="section-title-center d1">Mari Berkolaborasi</h2>
             <p className="kontak-sub d2">
               Isi form — pesan langsung terkirim ke email saya{' '}
-              <span style={{ color: '#3b82f6' }}>ardrasakhi390@gmail.coms</span>{' '}
+              <span style={{ color: '#3b82f6' }}>ardrasakhi390@gmail.com</span>{' '}
               {/* ungu sangat muda */}
             </p>
           </div>
