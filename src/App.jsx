8090eb8ffb7ad2d769b1a07ffa5d3e7c320c78d5
sakhiard <ardrasakhi390@gmail.com>
2026-05-18 22:10:00 +0700

    update fix 7 foto

--- src/App.jsx
+++ src/App.jsx
@@ -701,7 +701,7 @@ export default function App() {
               />
             </div>
             <div className="form-group">
-              <label>Email</label>
+              <label>Emaisasasl</label>
               <input
                 type="email"
                 name="email"
