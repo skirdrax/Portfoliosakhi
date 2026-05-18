dfae659f038bc82aa46274f425febab4b4691274
sakhiard <ardrasakhi390@gmail.com>
2026-05-18 22:10:15 +0700

    update fix 7 fotos

--- src/App.jsx
+++ src/App.jsx
@@ -701,7 +701,7 @@ export default function App() {
               />
             </div>
             <div className="form-group">
-              <label>Emaisasasl</label>
+              <label>Emaisasaslsssssswwqw</label>
               <input
                 type="email"
                 name="email"
