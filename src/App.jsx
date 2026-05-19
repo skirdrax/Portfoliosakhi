815ad441e610c6da147f9ab38d43a9401eeb2061
sakhiard <ardrasakhi390@gmail.com>
2026-05-19 09:40:58 +0700

    update fix 8

--- src/App.jsx
+++ src/App.jsx
@@ -701,7 +701,7 @@ export default function App() {
               />
             </div>
             <div className="form-group">
-              <label>Email</label>
+              <label>Emasl</label>
               <input
                 type="email"
                 name="email"
