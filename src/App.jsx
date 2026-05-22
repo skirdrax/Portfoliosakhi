2c4d66ca3dbfb7a87571a7c0eda9915f0707f8f2
sakhiard <ardrasakhi390@gmail.com>
2026-05-22 21:57:35 +0700

    dast/Zap-Rerepetshop-report

--- src/App.jsx
+++ src/App.jsx
@@ -691,7 +691,7 @@ export default function App() {
               style={{ display: 'none' }}
             />
             <div className="form-group">
-              <label>Nama Anda </label>
+              <label>Nama Andass </label>
               <input
                 type="text"
                 name="name"
