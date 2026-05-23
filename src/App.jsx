84acb0a2ac9e8d12766e0a50aaf1357560a494df
sakhiard <ardrasakhi390@gmail.com>
2026-05-23 18:53:20 +0700

    bismillah

--- src/App.jsx
+++ src/App.jsx
@@ -695,7 +695,7 @@ export default function App() {
               <input
                 type="text"
                 name="name"
-                placeholder="Masukkan nama anda ya"
+                placeholder="Masukkan nama anda"
                 className="contact-input"
                 required
               />
