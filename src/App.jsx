5e180e18e45464c7d7ae05a932a03915ea2040b5
sakhiard <ardrasakhi390@gmail.com>
2026-05-23 18:53:44 +0700

    bismillah

--- src/App.jsx
+++ src/App.jsx
@@ -691,7 +691,7 @@ export default function App() {
               style={{ display: 'none' }}
             />
             <div className="form-group">
-              <label>Nama Andaa </label>
+              <label>Nama Anda </label>
               <input
                 type="text"
                 name="name"
