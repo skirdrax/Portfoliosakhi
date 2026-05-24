280c4ea54d5b588e4640a87f6a5c2110d43942c1
sakhiard <ardrasakhi390@gmail.com>
2026-05-24 21:17:47 +0700

    mitigasi perbaikan

--- src/App.jsx
+++ src/App.jsx
@@ -691,7 +691,7 @@ export default function App() {
               style={{ display: 'none' }}
             />
             <div className="form-group">
-              <label>Nama Anda </label>
+              <label>Nama Andassss </label>
               <input
                 type="text"
                 name="name"
