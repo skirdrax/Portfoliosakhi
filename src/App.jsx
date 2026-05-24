cf229a171959b9449c8977f5116718aec6b7606f
sakhiard <ardrasakhi390@gmail.com>
2026-05-24 21:19:18 +0700

    gas ha

--- src/App.jsx
+++ src/App.jsx
@@ -691,7 +691,7 @@ export default function App() {
               style={{ display: 'none' }}
             />
             <div className="form-group">
-              <label>Nama Anda </label>
+              <label>Nama Andasssss </label>
               <input
                 type="text"
                 name="name"
