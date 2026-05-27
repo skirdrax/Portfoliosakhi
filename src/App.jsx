6e661a51f7b162f03db68e11583427fcc4e1a7ff
sakhiard <ardrasakhi390@gmail.com>
2026-05-27 18:20:52 +0700

    gas ha

--- src/App.jsx
+++ src/App.jsx
@@ -710,7 +710,7 @@ export default function App() {
               />
             </div>
             <div className="form-group">
-              <label>Pesan ssss</label>
+              <label>Pesan</label>
               <textarea
                 name="message"
                 rows="6"
