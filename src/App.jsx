c417d274926979ca7d6bb6282fcccf100f7bbeb6
sakhiard <ardrasakhi390@gmail.com>
2026-05-28 16:25:17 +0700

    kode otp email

--- src/App.jsx
+++ src/App.jsx
@@ -710,7 +710,7 @@ export default function App() {
               />
             </div>
             <div className="form-group">
-              <label>Pesan</label>
+              <label>Pesanssssss addad asa </label>
               <textarea
                 name="message"
                 rows="6"
