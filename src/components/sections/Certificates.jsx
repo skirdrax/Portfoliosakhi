f639f12bf180b2552edf46dc25e89f8b08d32f40
sakhiard <ardrasakhi390@gmail.com>
2026-06-01 20:44:10 +0700

    update besar 2

--- src/components/sections/Certificates.jsx
+++ src/components/sections/Certificates.jsx
@@ -27,7 +27,7 @@ export default function Certificates() {
         slidesPerView={1}
         navigation
         pagination={{ clickable: true }}
-        autoplay={{ delay: 3000, disableOnInteraction: false }}
+        autoplay={{ delay: 2000, disableOnInteraction: false }}
         breakpoints={{
           640: { slidesPerView: 2 },
           1024: { slidesPerView: 3 },
