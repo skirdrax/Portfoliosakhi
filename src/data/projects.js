a176ca2027f3d7ca5535ab6fd23afea2e1bfe086
sakhiard <ardrasakhi390@gmail.com>
2026-06-04 20:58:38 +0700

    satu

--- src/data/projects.js
+++ src/data/projects.js
@@ -47,7 +47,7 @@ export const listProyek = [
     gambar: Proyek5,
     nama: 'Pocket Radar App',
     kategori: 'Mobile',
-    desk: 'Apsslikssasi Pencssatat keuassngan prissbadi yang memsssbantu pengssguna mengelssola pengessslussaran dssan pemasukanssss dengan mudah dan efisien serta memberikan analisis keuangan yang informatif  .',
+    desk: 'Aplikssasi Pencssatat keuassngan prissbadi yang memsssbantu pengssguna mengelssola pengessslussaran dssan pemasukanssss dengan mudah dan efisien serta memberikan analisis keuangan yang informatif  .',
     tools: ['Flutter', 'Firebase', 'Figma', 'Canva'],
     link: 'https://drive.google.com/drive/folders/1JNprvN24hUdXYlZN9OUHJIXlKQSpjiV6?usp=sharing',
   },
