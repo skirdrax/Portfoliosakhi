5072e32bdb84f066c36f15f2b4e26fe8eddce05e
sakhiard <ardrasakhi390@gmail.com>
2026-06-05 21:07:27 +0700

    final

--- src/data/tools.js
+++ src/data/tools.js
@@ -4,7 +4,7 @@ import Tools3 from '/assets/tools/laravel.png';
 import Tools4 from '/assets/tools/MySQL.png';
 import Tools5 from '/assets/tools/bootstrap.png';
 import Tools6 from '/assets/tools/js.png';
-import Tools7 from '/assets/tools/nodejs.png';
+import Tools7 from '/assets/tools/flutter.png';
 import Tools8 from '/assets/tools/github.png';
 import Tools9 from '/assets/tools/ubuntu.jpeg';
 import Tools10 from '/assets/tools/canva.png';
@@ -18,7 +18,7 @@ export const listTools = [
   { id: 4, gambar: Tools4, nama: 'MySQL', ket: 'Database' },
   { id: 5, gambar: Tools5, nama: 'Bootstrap', ket: 'Framework' },
   { id: 6, gambar: Tools6, nama: 'Javascript', ket: 'Language' },
-  { id: 7, gambar: Tools7, nama: 'Node JS', ket: 'Javascript Runtime' },
+  { id: 7, gambar: Tools7, nama: 'Flutter', ket: 'Mobile Framework' },
   { id: 8, gambar: Tools8, nama: 'Github', ket: 'Repository' },
   { id: 9, gambar: Tools9, nama: 'Ubuntu', ket: 'Operating System' },
   { id: 10, gambar: Tools10, nama: 'Canva', ket: 'Design App' },
