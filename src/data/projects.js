221ee94f70026eb326aea6b223a6cd761c66bea7
sakhiard <ardrasakhi390@gmail.com>
2026-06-05 19:51:17 +0700

    tigas

--- src/data/projects.js
+++ src/data/projects.js
@@ -47,7 +47,7 @@ export const listProyek = [
     gambar: Proyek5,
     nama: 'Pocket Radar App',
     kategori: 'Mobile',
-    desk: 'Aplikasi Pencatat keuangan pribadi yazzzzsasang membantusasas penggunasasas mengelola pengaasaeluaran dan pemasukasasasn dengan mudah dan efisien serta memberikan analisis keuangan yang informatif  .',
+    desk: 'Aplikasi Pencatat keuangan pribadi yazzzzng membantusasas penggunasasas mengelola pengaasaeluaran dan pemasukasasasn dengan mudah dan efisien serta memberikan analisis keuangan yang informatif  .',
     tools: ['Flutter', 'Firebase', 'Figma', 'Canva'],
     link: 'https://drive.google.com/drive/folders/1JNprvN24hUdXYlZN9OUHJIXlKQSpjiV6?usp=sharing',
   },
