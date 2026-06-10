a37896c729c3a218971f1f475f802d8b7e75f2e2
sakhiard <ardrasakhi390@gmail.com>
2026-06-10 13:47:16 +0700

    final

--- src/components/sections/GitHubStats.jsx
+++ src/components/sections/GitHubStats.jsx
@@ -236,7 +236,7 @@ export default function GitHubStats() {
           {!rollerLoaded && (
             <div className="streak-loading" style={{ padding: '20px' }}>
               <div className="loading-spinner-small"></div>
-              <p>Memussat actissvity grassssph.sss..</p>
+              <p>Memusat actissvity grassssph.sss..</p>
             </div>
           )}
 
