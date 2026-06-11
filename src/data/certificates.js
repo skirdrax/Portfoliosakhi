1621c754eb9718298615e1ac4a9a1f4ae8942c0e
sakhiard <ardrasakhi390@gmail.com>
2026-06-11 21:21:26 +0700

    final

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -42,7 +42,7 @@ export const certificates = [
   {
     id: 6,
     title: 'Dasar AI',
-    issuer: 'Dicoding Indonesia',
+    issuer: 'Dicoding Indonesias',
     date: '2024',
     image: '/assets/sertifikat/serti6.png',
     link: 'https://academy.oracle.com/certificates/xxx',
