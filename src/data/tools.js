57e2b2e423635131ceae8141923c14553a48aa94
sakhiard <ardrasakhi390@gmail.com>
2026-08-13 14:27:27 +0700

    fix: add all tools icons

--- src/data/tools.js
+++ src/data/tools.js
@@ -10,6 +10,11 @@ import Tools9 from '/assets/tools/ubuntu.jpeg';
 import Tools10 from '/assets/tools/canva.png';
 import Tools11 from '/assets/tools/figma.png';
 import Tools12 from '/assets/tools/gsc.webp';
+import Tools13 from '/assets/tools/python.png';
+import Tools14 from '/assets/tools/aapanel.png';
+import Tools15 from '/assets/tools/arduino.png';
+import Tools16 from '/assets/tools/postman.png';
+import Tools17 from '/assets/tools/postgre-mysql.png';
 
 export const listTools = [
   { id: 1, gambar: Tools1, nama: 'Visual Studio Code', ket: 'Code Editor' },
@@ -17,11 +22,16 @@ export const listTools = [
   { id: 3, gambar: Tools3, nama: 'Laravel', ket: 'Framework' },
   { id: 4, gambar: Tools4, nama: 'MySQL', ket: 'Database' },
   { id: 5, gambar: Tools5, nama: 'Bootstrap', ket: 'Framework' },
-  { id: 6, gambar: Tools6, nama: 'Javascript', ket: 'Language' },
+  { id: 6, gambar: Tools6, nama: 'JavaScript', ket: 'Language' },
   { id: 7, gambar: Tools7, nama: 'Flutter', ket: 'Mobile Framework' },
-  { id: 8, gambar: Tools8, nama: 'Github', ket: 'Repository' },
+  { id: 8, gambar: Tools8, nama: 'GitHub', ket: 'Repository' },
   { id: 9, gambar: Tools9, nama: 'Ubuntu', ket: 'Operating System' },
   { id: 10, gambar: Tools10, nama: 'Canva', ket: 'Design App' },
   { id: 11, gambar: Tools11, nama: 'Figma', ket: 'Design App' },
   { id: 12, gambar: Tools12, nama: 'Google Search Console', ket: 'SEO Tool' },
+  { id: 13, gambar: Tools13, nama: 'Python', ket: 'Language' },
+  { id: 14, gambar: Tools14, nama: 'AApanel', ket: 'Server Panel' },
+  { id: 15, gambar: Tools15, nama: 'Arduino', ket: 'IoT Platform' },
+  { id: 16, gambar: Tools16, nama: 'Postman', ket: 'API Testing' },
+  { id: 17, gambar: Tools17, nama: 'MySQL & PostgreSQL', ket: 'Database' },
 ];
